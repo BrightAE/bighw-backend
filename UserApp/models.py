763f5bb4fba8fc2de89584e09d683d7299374ab7
HwwwwwH <hezhihui_thu@163.com>
2020-09-09 11:01:58 +0800

    test

---
+++ UserApp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=200)
+    mail = models.EmailField()
+    student_id = models.CharField()
+    authority = models.CharField()
+    active = models.BooleanField(default=False)
+    rand_str = models.CharField(max_length=233)
