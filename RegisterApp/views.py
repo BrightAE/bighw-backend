15c4dd78c99aa8de8ef5d26caff022fecc25b38b
2018013422 <1764320683@qq.com>
2020-09-10 16:20:52 +0800

    update JWT in RegisterApp

--- RegisterApp/views.py
+++ RegisterApp/views.py
@@ -23,7 +23,7 @@ def logon(request):
     if request.method != 'POST':
         return JsonResponse({"error": "require POST"})
     all_index = ['username', 'password', 'student_id', 'email', 'contact']
-    print(request.POST['email'])
+    # print(request.POST['email'])
     for index in all_index:
         if index not in request.POST or len(request.POST[index]) == 0:
             return JsonResponse({"error": "invalid parameters"})
@@ -51,11 +51,11 @@ def logon(request):
     test.rand_str = ''
     test.save()
 
-    items = User.objects.all()
-    for item in items:
-        print(item.id, ' ', item.username, ' ', item.password, ' ', item.email,
-              ' ', item.student_id, ' ', item.authority, ' ',
-              item.active)
+    # items = User.objects.all()
+    # for item in items:
+    #     print(item.id, ' ', item.username, ' ', item.password, ' ', item.email,
+    #           ' ', item.student_id, ' ', item.authority, ' ',
+    #           item.active)
 
     rand_str = RegisterApp.send_email.get_rand_str(40)
     RegisterApp.send_email.send_register_email(test.email, rand_str)
@@ -116,8 +116,9 @@ def login(request):
     rand_str = get_rand_str(88)
     saved_user.rand_str = rand_str
     saved_user.save()
-    response = JsonResponse({'message': 'ok'})
-    response.set_cookie('session_id', rand_str)
+    # response = JsonResponse({'message': 'ok'})
+    # response.set_cookie('session_id', rand_str)
+    response = JsonResponse({'message': 'ok', 'jwt': rand_str})
     return response
 
 
@@ -127,9 +128,9 @@ def logout(request):
     if not check_login(request):
         return JsonResponse({"error": "please login"})
     # print(request)
-    # print(request.COOKIES['session_id'])
+    # print(request.META['HTTP_JWT'])
     # return JsonResponse({'error': 'TEST'})
-    saved_user = User.objects.get(rand_str=request.COOKIES['session_id'])
+    saved_user = User.objects.get(rand_str=request.META['HTTP_JWT'])
     print("success logout: ", saved_user.username)
     response = JsonResponse({'message': 'ok'})
     saved_user.rand_str = ''
@@ -143,7 +144,7 @@ def query_all(request):
 
     if not check_login(request):
         return JsonResponse({"error": "please login"})
-    saved_user = User.objects.get(rand_str=request.COOKIES['session_id'])
+    saved_user = User.objects.get(rand_str=request.META['HTTP_JWT'])
     if saved_user.authority != 'admin':
         return JsonResponse({"error": "not admin"})
     print("admin querying ALL: ", saved_user.username)
@@ -175,7 +176,7 @@ def set_authority(request):
 
     if not check_login(request):
         return JsonResponse({"error": "please login"})
-    saved_user = User.objects.get(rand_str=request.COOKIES['session_id'])
+    saved_user = User.objects.get(rand_str=request.META['HTTP_JWT'])
     if saved_user.authority != 'admin':
         return JsonResponse({"error": "not admin"})
     print("admin setting authority: ", saved_user.username)
@@ -202,7 +203,7 @@ def delete_user(request):
 
     if not check_login(request):
         return JsonResponse({"error": "please login"})
-    saved_user = User.objects.get(rand_str=request.COOKIES['session_id'])
+    saved_user = User.objects.get(rand_str=request.META['HTTP_JWT'])
     if saved_user.authority != 'admin':
         return JsonResponse({"error": "not admin"})
     print("admin deleting: ", saved_user.username)
@@ -223,7 +224,7 @@ def user_info(request):
 
     if not check_login(request):
         return JsonResponse({"error": "please login"})
-    user = User.objects.get(rand_str=request.COOKIES['session_id'])
+    user = User.objects.get(rand_str=request.META['HTTP_JWT'])
 
     return JsonResponse({'user_id': user.id, 'student_id': user.student_id, 'username': user.username,
                          'authority': user.authority})
@@ -235,7 +236,7 @@ def decide_auth_request(request):
 
     if not check_login(request):
         return JsonResponse({"error": "please login"})
-    saved_user = User.objects.get(rand_str=request.COOKIES['session_id'])
+    saved_user = User.objects.get(rand_str=request.META['HTTP_JWT'])
     if saved_user.authority != 'admin':
         return JsonResponse({"error": "not admin"})
     print("admin deciding auth: ", saved_user.username)
@@ -262,7 +263,7 @@ def add_auth_request(request):
 
     if not check_login(request):
         return JsonResponse({"error": "please login"})
-    saved_user = User.objects.get(rand_str=request.COOKIES['session_id'])
+    saved_user = User.objects.get(rand_str=request.META['HTTP_JWT'])
     if saved_user.authority != 'user':
         return JsonResponse({"error": "not a normal user"})
     print("user requesting auth: ", saved_user.username)
@@ -281,204 +282,8 @@ def add_auth_request(request):
     return JsonResponse({'message': 'ok'})
 
 
+# def check_login(request):
+#     return 'session_id' in request.COOKIES and len(request.COOKIES['session_id']) > 0\
+#            and User.objects.filter(rand_str=request.COOKIES['session_id']).exists()
 def check_login(request):
-    return 'session_id' in request.COOKIES and len(request.COOKIES['session_id']) > 0\
-           and User.objects.filter(rand_str=request.COOKIES['session_id']).exists()
-
-
-#
-#
-# def record_add(request):
-#     if request.method != 'POST':
-#         return JsonResponse({"error": "require POST"})
-#     if ('name' not in request.POST) or ('time' not in request.POST) or ('content' not in request.POST):
-#         return JsonResponse({'error': 'invalid parameters'})
-#
-#     saved_session = MySession.objects.filter(sessionID=request.COOKIES['session_id'])
-#     if not saved_session.exists():
-#         return JsonResponse({"error": "please login"})
-#     saved_session = MySession.objects.get(sessionID=request.COOKIES['session_id'])
-#     print("user adding record: ", saved_session.username)
-#     add_name = request.POST['name']
-#     add_time = request.POST['time']
-#     add_content = request.POST['content']
-#     if min(len(add_name), len(add_content)) == 0:
-#         return JsonResponse({'error': 'invalid parameters'})
-#     print(type(add_time))
-#     try:
-#         add_time = int(add_time)
-#     except:
-#         return JsonResponse({'error': 'invalid parameters'})
-#     if add_time <= 0:
-#         return JsonResponse({'error': 'invalid parameters'})
-#     temp = MyData()
-#     temp.name = add_name
-#     temp.time = add_time
-#     temp.content = add_content
-#     temp.user = saved_session.username
-#     temp.save()
-#
-#     items = MyData.objects.all()
-#     for item in items:
-#         print(item.id, ' ', item.name, ' ', item.time, ' ', item.content, ' ', item.user)
-#
-#     return JsonResponse({'record_id': temp.id})
-#
-#
-# def record_delete(request, del_id):
-#     if request.method != 'POST':
-#         return JsonResponse({"error": "require POST"})
-#
-#     saved_session = MySession.objects.filter(sessionID=request.COOKIES['session_id'])
-#     if not saved_session.exists():
-#         return JsonResponse({"error": "please login"})
-#     saved_session = MySession.objects.get(sessionID=request.COOKIES['session_id'])
-#     print("user deleting record: ", saved_session.username)
-#
-#     try:
-#         del_id = int(del_id)
-#     except:
-#         return JsonResponse({'error': 'invalid parameters'})
-#     if del_id <= 0:
-#         return JsonResponse({'error': 'invalid parameters'})
-#
-#     saved_data = MyData.objects.filter(id=del_id)
-#     if not saved_data.exists():
-#         return JsonResponse({"error": "unknown record"})
-#     saved_data = MyData.objects.get(id=del_id)
-#     print("content to delete:  ", saved_data.content)
-#     print("    time:  ", saved_data.time)
-#     if (saved_data.time == -233) or (saved_data.user != saved_session.username):
-#         return JsonResponse({"error": "unknown record"})
-#     saved_data.time = -233
-#     saved_data.save()
-#     return JsonResponse({'record_id': del_id})
-#
-#
-# def record_update(request, upd_id):
-#     if request.method != 'POST':
-#         return JsonResponse({"error": "require POST"})
-#
-#     saved_session = MySession.objects.filter(sessionID=request.COOKIES['session_id'])
-#     if not saved_session.exists():
-#         return JsonResponse({"error": "please login"})
-#     saved_session = MySession.objects.get(sessionID=request.COOKIES['session_id'])
-#     print("user updating record: ", saved_session.username)
-#
-#     try:
-#         upd_id = int(upd_id)
-#     except:
-#         return JsonResponse({'error': 'invalid parameters'})
-#     if upd_id <= 0:
-#         return JsonResponse({'error': 'invalid parameters'})
-#
-#     saved_data = MyData.objects.filter(id=upd_id)
-#     if not saved_data.exists():
-#         return JsonResponse({"error": "unknown record"})
-#     saved_data = MyData.objects.get(id=upd_id)
-#     print("content to update:  ", saved_data.content)
-#     print("    time:  ", saved_data.time)
-#     if (saved_data.time == -233) or (saved_data.user != saved_session.username):
-#         return JsonResponse({"error": "unknown record"})
-#
-#     for index in request.POST:
-#         if index != 'name' and index != 'time' and index != 'content':
-#             return JsonResponse({"error": "unknown record filed"})
-#         # print(index)
-#
-#     for index in request.POST:
-#         if index == 'name':
-#             saved_data.name = request.POST['name']
-#         if index == 'time':
-#             saved_data.time = request.POST['time']
-#         if index == 'content':
-#             saved_data.content = request.POST['content']
-#
-#     saved_data.save()
-#
-#     items = MyData.objects.all()
-#     for item in items:
-#         print(item.id, ' ', item.name, ' ', item.time, ' ', item.content, ' ', item.user)
-#     return JsonResponse({'record_id': upd_id})
-#
-#
-# def record_get(request, get_id):
-#     if request.method != 'GET':
-#         return JsonResponse({"error": "require GET"})
-#
-#     saved_session = MySession.objects.filter(sessionID=request.COOKIES['session_id'])
-#     if not saved_session.exists():
-#         return JsonResponse({"error": "please login"})
-#     saved_session = MySession.objects.get(sessionID=request.COOKIES['session_id'])
-#     print("user getting record: ", saved_session.username)
-#
-#     saved_data = MyData.objects.filter(id=get_id)
-#     if not saved_data.exists():
-#         return JsonResponse({"error": "unknown record"})
-#     saved_data = MyData.objects.get(id=get_id)
-#     if (saved_data.time == -233) or (saved_data.user != saved_session.username):
-#         return JsonResponse({"error": "unknown record"})
-#
-#     return JsonResponse({'record_id': get_id, 'name': saved_data.name, 'content': saved_data.content,
-#                          'time': get_peking_time(saved_data.time)})
-#
-#
-# def record_query(request):
-#     if request.method != 'GET':
-#         return JsonResponse({"error": "require GET"})
-#
-#     saved_session = MySession.objects.filter(sessionID=request.COOKIES['session_id'])
-#     if not saved_session.exists():
-#         return JsonResponse({"error": "please login"})
-#     saved_session = MySession.objects.get(sessionID=request.COOKIES['session_id'])
-#     print("user querying record: ", saved_session.username)
-#
-#     upd_name = request.GET.get('name')
-#     result_list = []
-#     if upd_name is None or len(upd_name) == 0:
-#         items = MyData.objects.all()
-#         for item in items:
-#             if item.time >= 0:
-#                 result_list.append({'record_id': item.id, 'name': item.name, 'content': item.content,
-#                                     'time': get_peking_time(item.time)})
-#     else:
-#         items = MyData.objects.all()
-#         for item in items:
-#             if item.name.find(upd_name) >= 0 and item.time >= 0:
-#                 # print(item.time,"type:",type(item.time))
-#                 result_list.append({'record_id': item.id, 'name': item.name, 'content': item.content,
-#                                     'time': get_peking_time(item.time)})
-#
-#     return JsonResponse({'list': result_list})
-#
-#
-# # def invalid_url():
-# #     return HttpResponse(status=404)
-#
-#
-# def get_rand_str(length):
-#     rand_str = ""
-#     for i in range(length):
-#         rand_str += random.choice(string.ascii_letters + string.digits)
-#     return rand_str
-#
-#
-# def create_session(rand_str, username):
-#     print("CREATING:  ", username, ' ', rand_str)
-#     temp = MySession()
-#     temp.sessionID = rand_str
-#     temp.username = username
-#     temp.save()
-#
-#     items = MySession.objects.all()
-#     for item in items:
-#         print(item.sessionID, ' ', item.username)
-#
-#
-# def get_peking_time(num):
-#     time_stamp = num/1000.0
-#     time_array = time.localtime(time_stamp)
-#     # print(time_array)
-#     style_time = time.strftime("%Y-%m-%d %H:%M:%S", time_array) + '.' + str(num%1000).zfill(3)
-#     # print(style_time)
-#     return style_time
+    return 'HTTP_JWT' in request.META and User.objects.filter(rand_str=request.META['HTTP_JWT']).exists()
