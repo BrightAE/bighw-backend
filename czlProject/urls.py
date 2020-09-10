1bdbe090617c485c831c5219138ada3198b20c17
BrightAE <brighae.hzm@gmail.com>
2020-09-10 19:05:39 +0800

    fix /api/rent

--- czlProject/urls.py
+++ czlProject/urls.py
@@ -45,8 +45,8 @@ urlpatterns = [
     url(r'^api/equip/request/add', EquipmentApp.views.equip_request_add),
     url(r'^api/rent/query$', RentApp.views.rent_query),
     url(r'^api/rent/request/query$', RentApp.views.rent_request_query),
-    url(r'^api/request/decide$', RentApp.views.rent_request_decide),
-    url(r'^api/request/delete$', RentApp.views.rent_request_delete),
-    url(r'^api/request/add$', RentApp.views.rent_request_add),
+    url(r'^api/rent/request/decide$', RentApp.views.rent_request_decide),
+    url(r'^api/rent/request/delete$', RentApp.views.rent_request_delete),
+    url(r'^api/rent/request/add$', RentApp.views.rent_request_add),
     url(r'^api/rent/confirm$', RentApp.views.rent_confirm),
 ]
